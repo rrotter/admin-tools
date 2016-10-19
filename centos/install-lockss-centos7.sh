97b36082b25e1f412dc2b3e808fc398d71114972
Daniel Vargas <dlvargas@stanford.edu>
2016-10-18 17:42:29 -0700

    Initial commit

---
+++ centos/install-lockss-centos7.sh
@@ -0,0 +1,101 @@
+#!/bin/sh
+function writekeys {
+( base64 -id | gunzip -c | cat > $1 ) <<'EOF'
+H4sIACv/300AA32Wx661xhKF5/cpPEc2OQ0smZzDJsOMnMMmw9Pf89sem1lLtLq66uu11rY1vxfb
+9hvz87Go+aYc3Oeo8bPiWMa8yVz5YucplMW4EG6ZQClLGFogHY3BG1WxlGlGIAEU+CE51VFn5JzA
+hlCUvksfg74swUFQzxKDz/vQZwcImspmgjN/Tr3Ce9Cpl5laNcL3Hqr5YBloYoXlFMCk33CJ9PLc
+6jTeY1/iVum2bYI6wMKTp6ck7WiEUGAEenJtKomneH/VL354/lQB9yPHD+YPgr0c9S4T9sp9ae7z
+84PCggOxwjIx6dvemcA37F41CLSEUD/kwJUi9xwAcieMgWiHma1Z29TYilv6cRUTGE1CbH/V11t4
+kN9JVBo5s2Dy0KkE5DQTjZlTS2m+jJCWuQKflQ0PBe+LY10nU7UWwctvwzQdEgrLr5SZDQm5F8bR
+4hdeqKoCD3tqT9YYK75E/U/160IKU8NpjFd8MEyjNFilI0ZDno8MNlYkDuEA00HgQ1ewmBJaXGEg
+F4FgNl21CDMv68u7EQYgbr0pCZxsf7RMu4odqiW9Fur95B+dQDwZRuPPPApDJDrYmAmt4AvhkX13
+eyIzh2bjHMWoHJaeOATQuuTJNUWC1lTnph4p64g3m5vka9GvP//8bU+Hv4b5/t/2X1AZQeAxwfE5
+NTaChEUtd3je19O25aptfAgodafFNyO9xWspEO+aheJxlefN/C42Sq9a3Mk5hii1QV/P82FJHbMH
+WLwriRl2vYR582/mjm6WKYWc64GL17O4SXyhSyFQK7TsgJ6Tn+uI+cpafmTY3pjvVrPCyTJJxMnN
+fdpiosrtME//QMU1rjEcRASTdxaGeAVYNFP2cP9F3L+hEpiru4E4bjhwcoAwjsOve2marOa4n61A
+lPGKbKXMJnXA3FVLSM3gY6bhZUVzE1kPORU1GzyJJl9qstQKUpkutqB4m0WV5Iu66okV3q62xCiw
+K5sbdk5lTSPueY8Gronm/DbAWgCIsPMDCXfSc4yhIbxCQyaFgeIwSH1VXRo4ksTFB/s1A0aMQNgx
+GLCzTaLW26KfKCkVdPKSChRFKnK+RotjPZfoy8ahwZVW7ah0J1OwtXdSyE7FpJK9CIPoTIhYnjCa
+ouhIydOhsomAePYDywutkjQDynW+apNodu5MKbYdESBBT+CuBmSjOo6T+VcGedgdQ8yd/jRO08Lx
+aBpNN11rhjl8hP78rdia9a+tOfZ9KP+DLIFlLEJ32XqC2vx1PFKRFh6JIggDgLmuugg08Vw4Z6M6
+u2LQ2Jv8fsAvGuvR6fu7f4+q/Gx9qjIxzS4qRB5o5metlngeJ8AEZG0AkV+jF0/UHZWKN32TiJeD
+nTowIVvopfn4+aOPwsGIhPiOXrAU1Mvd1+qX1fxTkZez3TKY3V036kNPXjyHA7qDgggMOJjGcGCr
+0CffdNyhoG8NoLDfx6ls5BXt8AaOt4NtTe/XSToBFLk8bTOkJWEAmri1bLOnmw5SzwHtQWcb2MsA
+r/qZJq0KHln6jqMfVAg8j/DpWmJ5LZd0EhRAq7AVafO9UvKCL3tMlqka2fBxtN/IypAKBdLTmz7b
+r1YHjH1KJRmkeD/ZoUKNAMWar8mLpYP8eiUs8+E+7qJhk05tHoUtvn6jNdxdnVgbaDBSmL6Q7P4V
+t1SmC3hXu4ubutZKnZpOp6KCcvop9fdHClSAo8zcukrDCeGEz4LuzbOGM7IRYXNcErOEiIx2tTZJ
+uh41OkPLo33MqHEM2dYPAXshFC+QSnT7N4wpnCTq8lBuF/1G6zeXCSrVu7agCesWpUdusTZAyB+e
+TzOctQKA+MkZ5TceXjMm12qRxIUatF4j/WcJiNeBHrQBmDWkFexnQuqQb3b6VcQpPz1Q/QBeUCEf
+rLLMOfMeG6uD69EUxnd056B1Ro7OCU/8Q66P3rRg+iCrIQbc7sNtmGdeNvMJCYqlrQ3Jd6s6T0cl
+iX96y5ZGRUl41Gvis8ax6yACdNufEEdFB5/fFzMgjvpEbzGprUZgG/vQ8ocImdo4WyirE+NH+nqA
+htoopQQzphW5212NVqarOPc+nyJ7SXa6Zz2t32iVQEFtBxKU4XKIYXtLqrLyVcElgZmShIQ5dKn4
+2RqFYTEs3qetBEa+oNS4HbRz271zpWRsx/BA0CNZVxQ1b5LZVMYeOxvLpj67IdMRPxJ0RKUgPYyk
+RVG3gC18a59XC+SHxr5QJL8zQDHI9DwoQBzpmDRS4A3x8WEsyCAA3/mOmSiFDEMCWpKzcNn9eHqp
+wbWS2HukeKWjk7pU2zvmMg9oCh+x2psslGlMqZsDmldj+7Grps3+2o7h92HO+21DoT+2PZ2qeS3+
+KIvjv13MDPy1Q0DP07GvVTK4nubTLEf7CzSOAt4WxuDv6z+8OBWMb97ox+TSoTmqtIJ1rfBRDxFL
+iR+dmtJ/olICHaSboXrwEL0q98gUEOHKzNhEb100uLMQgiVX0ZHgg8Fx647Xee+U4Jc1Ssb1bV1K
++Oq3kdpT/vLmFwNwJf0x1SbyzXQI/nWxA79yb/rRJrzRLflyy3ZqY1Nqi/L6x8VaP8wq1+BItioK
++CbKOsaJQHRRbGzGIJaczjhsf5rvx2R+DvqQDTbFZMjxjjx/9GR92TLL4wemZxy8AzDZiVagUKs6
+NP1knllpcyqFvuTjFjxIRo9akrfRmr66vefc9OTaN3y1f4xPEiISZKRyhe3UGSQkEkY3eAUhWfd7
+MAIaGP09A0ZAzIztOk14nCxpKwJY73RhZTOky+/c1kZfdPVPCIRRUl8ztFzj4Mkjy1ouulN4WpP2
+/Hs6+2RHdlKa1iAB3U5CoXmfhLIARMr3C/wJaZ339O00iOOoA7sfvADKPpMgn4OguKoBlFv0aRrn
+ZL0HmfMd+O4QTIYoXOXMsoyfLMxlFLl+XCyd2vJXOvpF2h/zWv83Xlr5E4Tv0vOBWgjFEbbM1sdi
+Vm6uFXr6sk7nBK1p80T1gRXEu05jEqHFKcuowkkRNnx9rV+a8gYcZXO5vXO5DwhEmxC9KcATPr8u
+SbmFtRZaqhULWU56CFEG1qFMI2+6qwB8jJ+tWE3U6PeUv6vaufgqVZIoC9KNYzfdsLdYfJYyXv/F
+a7swMAajfa2+2h4UjNrgOVfQi1n8HZIYJe93JHA/X2PVZEKZHfIJZKpklcvh71EAt+2aWWTpMGA8
+7IXoki+IWR9gtufVKhy9Yp8iQXrKRQSL820olxHkOGOxlpbM5kPvE7QE3cc9UFg/eZ5fcVDldiOr
+we3tvVQNrb7gnluDnR9B9AGJwQLIOJ6J47NCqQMxycquHAn/da+q/rteLpfbNtvfNlKrDwdKcdLX
+KM6lq+nVUNI9FqT0bU35Ok1A2WG53+W5Agmt+gzFGqwYV6hz1uI74ITwTQSp3IVhgTSKOoREREDk
+WzV6fUrIYJBMq9JwFViuey+U3XMgsxyRgyIpMGqrG/jXZzFmY3/f/npPd+ykNGKQHO+p6khpxaB/
+Je+0SMf1r3ye9nLa4f/9HyASnPvWDQAA
+EOF
+}
+
+function createuser {
+	useradd -r -m ${1}
+
+	mkdir -p /home/${1}/.ssh
+	writekeys /home/${1}/.ssh/authorized_keys
+
+	chown -R ${1}:$(id -g ${1}) /home/${1}/.ssh
+	chmod 700 /home/${1}/.ssh
+	chmod 600 /home/${1}/.ssh/authorized_keys
+
+	test -x /sbin/restorecon && \
+		/sbin/restorecon /home/${1}/.ssh/authorized_keys
+}
+
+# Create the two LOCKSS users
+createuser 'lockss'
+createuser 'lcap'
+
+# Allow lcap user to sudo
+usermod -G wheel lcap
+echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+# Install the LOCKSS RPM repository
+cat > /etc/yum.repos.d/lockss.repo <<'EOF'
+[lockss] 
+name = LOCKSS Daemon Repository 
+baseurl=http://www.lockss.org/repo/ 
+gpgcheck=1
+EOF
+
+# Ensure the correct filesystem permissions on the repository file
+chmod 644 /etc/yum.repos.d/lockss.repo
+
+# Import the LOCKSS GPG key
+rpm --import http://www.lockss.org/LOCKSS-GPG-RPM-KEY
+
+# Install the LOCKSS daemon, OpenJDK and supporting tools
+yum -yq install lockss-daemon java-1.7.0-openjdk screen yum-cron 
+
+# Enable LOCKSS and other services
+systemctl enable lockss
+systemctl enable yum-cron
+#systemctl start yum-cron
+#systemctl start lockss
+
+# Append firewall rules
+firewall-cmd --zone=public --addport=8080-8086/tcp --permanent
+firewall-cmd --zone=public --addport=9729/tcp --permanent
