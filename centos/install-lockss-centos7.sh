a5c2b69eff5b33ffc861ab3790509f38d982613d
Clay Miller <claym@lockss.org>
2019-04-30 13:56:53 -0700

    added packages and yum-cron configuration

--- centos/install-lockss-centos7.sh
+++ centos/install-lockss-centos7.sh
@@ -1,62 +1,63 @@
 #!/bin/sh
-
-function WriteKeys {
+function writekeys {
 ( base64 -id | gunzip -c | cat > $1 ) <<'EOF'
-H4sIACv/300AA32Wx661xhKF5/cpPEc2OQ0smZzDJsOMnMMmw9Pf89sem1lLtLq66uu11rY1vxfb
-9hvz87Go+aYc3Oeo8bPiWMa8yVz5YucplMW4EG6ZQClLGFogHY3BG1WxlGlGIAEU+CE51VFn5JzA
-hlCUvksfg74swUFQzxKDz/vQZwcImspmgjN/Tr3Ce9Cpl5laNcL3Hqr5YBloYoXlFMCk33CJ9PLc
-6jTeY1/iVum2bYI6wMKTp6ck7WiEUGAEenJtKomneH/VL354/lQB9yPHD+YPgr0c9S4T9sp9ae7z
-84PCggOxwjIx6dvemcA37F41CLSEUD/kwJUi9xwAcieMgWiHma1Z29TYilv6cRUTGE1CbH/V11t4
-kN9JVBo5s2Dy0KkE5DQTjZlTS2m+jJCWuQKflQ0PBe+LY10nU7UWwctvwzQdEgrLr5SZDQm5F8bR
-4hdeqKoCD3tqT9YYK75E/U/160IKU8NpjFd8MEyjNFilI0ZDno8MNlYkDuEA00HgQ1ewmBJaXGEg
-F4FgNl21CDMv68u7EQYgbr0pCZxsf7RMu4odqiW9Fur95B+dQDwZRuPPPApDJDrYmAmt4AvhkX13
-eyIzh2bjHMWoHJaeOATQuuTJNUWC1lTnph4p64g3m5vka9GvP//8bU+Hv4b5/t/2X1AZQeAxwfE5
-NTaChEUtd3je19O25aptfAgodafFNyO9xWspEO+aheJxlefN/C42Sq9a3Mk5hii1QV/P82FJHbMH
-WLwriRl2vYR582/mjm6WKYWc64GL17O4SXyhSyFQK7TsgJ6Tn+uI+cpafmTY3pjvVrPCyTJJxMnN
-fdpiosrtME//QMU1rjEcRASTdxaGeAVYNFP2cP9F3L+hEpiru4E4bjhwcoAwjsOve2marOa4n61A
-lPGKbKXMJnXA3FVLSM3gY6bhZUVzE1kPORU1GzyJJl9qstQKUpkutqB4m0WV5Iu66okV3q62xCiw
-K5sbdk5lTSPueY8Gronm/DbAWgCIsPMDCXfSc4yhIbxCQyaFgeIwSH1VXRo4ksTFB/s1A0aMQNgx
-GLCzTaLW26KfKCkVdPKSChRFKnK+RotjPZfoy8ahwZVW7ah0J1OwtXdSyE7FpJK9CIPoTIhYnjCa
-ouhIydOhsomAePYDywutkjQDynW+apNodu5MKbYdESBBT+CuBmSjOo6T+VcGedgdQ8yd/jRO08Lx
-aBpNN11rhjl8hP78rdia9a+tOfZ9KP+DLIFlLEJ32XqC2vx1PFKRFh6JIggDgLmuugg08Vw4Z6M6
-u2LQ2Jv8fsAvGuvR6fu7f4+q/Gx9qjIxzS4qRB5o5metlngeJ8AEZG0AkV+jF0/UHZWKN32TiJeD
-nTowIVvopfn4+aOPwsGIhPiOXrAU1Mvd1+qX1fxTkZez3TKY3V036kNPXjyHA7qDgggMOJjGcGCr
-0CffdNyhoG8NoLDfx6ls5BXt8AaOt4NtTe/XSToBFLk8bTOkJWEAmri1bLOnmw5SzwHtQWcb2MsA
-r/qZJq0KHln6jqMfVAg8j/DpWmJ5LZd0EhRAq7AVafO9UvKCL3tMlqka2fBxtN/IypAKBdLTmz7b
-r1YHjH1KJRmkeD/ZoUKNAMWar8mLpYP8eiUs8+E+7qJhk05tHoUtvn6jNdxdnVgbaDBSmL6Q7P4V
-t1SmC3hXu4ubutZKnZpOp6KCcvop9fdHClSAo8zcukrDCeGEz4LuzbOGM7IRYXNcErOEiIx2tTZJ
-uh41OkPLo33MqHEM2dYPAXshFC+QSnT7N4wpnCTq8lBuF/1G6zeXCSrVu7agCesWpUdusTZAyB+e
-TzOctQKA+MkZ5TceXjMm12qRxIUatF4j/WcJiNeBHrQBmDWkFexnQuqQb3b6VcQpPz1Q/QBeUCEf
-rLLMOfMeG6uD69EUxnd056B1Ro7OCU/8Q66P3rRg+iCrIQbc7sNtmGdeNvMJCYqlrQ3Jd6s6T0cl
-iX96y5ZGRUl41Gvis8ax6yACdNufEEdFB5/fFzMgjvpEbzGprUZgG/vQ8ocImdo4WyirE+NH+nqA
-htoopQQzphW5212NVqarOPc+nyJ7SXa6Zz2t32iVQEFtBxKU4XKIYXtLqrLyVcElgZmShIQ5dKn4
-2RqFYTEs3qetBEa+oNS4HbRz271zpWRsx/BA0CNZVxQ1b5LZVMYeOxvLpj67IdMRPxJ0RKUgPYyk
-RVG3gC18a59XC+SHxr5QJL8zQDHI9DwoQBzpmDRS4A3x8WEsyCAA3/mOmSiFDEMCWpKzcNn9eHqp
-wbWS2HukeKWjk7pU2zvmMg9oCh+x2psslGlMqZsDmldj+7Grps3+2o7h92HO+21DoT+2PZ2qeS3+
-KIvjv13MDPy1Q0DP07GvVTK4nubTLEf7CzSOAt4WxuDv6z+8OBWMb97ox+TSoTmqtIJ1rfBRDxFL
-iR+dmtJ/olICHaSboXrwEL0q98gUEOHKzNhEb100uLMQgiVX0ZHgg8Fx647Xee+U4Jc1Ssb1bV1K
-+Oq3kdpT/vLmFwNwJf0x1SbyzXQI/nWxA79yb/rRJrzRLflyy3ZqY1Nqi/L6x8VaP8wq1+BItioK
-+CbKOsaJQHRRbGzGIJaczjhsf5rvx2R+DvqQDTbFZMjxjjx/9GR92TLL4wemZxy8AzDZiVagUKs6
-NP1knllpcyqFvuTjFjxIRo9akrfRmr66vefc9OTaN3y1f4xPEiISZKRyhe3UGSQkEkY3eAUhWfd7
-MAIaGP09A0ZAzIztOk14nCxpKwJY73RhZTOky+/c1kZfdPVPCIRRUl8ztFzj4Mkjy1ouulN4WpP2
-/Hs6+2RHdlKa1iAB3U5CoXmfhLIARMr3C/wJaZ339O00iOOoA7sfvADKPpMgn4OguKoBlFv0aRrn
-ZL0HmfMd+O4QTIYoXOXMsoyfLMxlFLl+XCyd2vJXOvpF2h/zWv83Xlr5E4Tv0vOBWgjFEbbM1sdi
-Vm6uFXr6sk7nBK1p80T1gRXEu05jEqHFKcuowkkRNnx9rV+a8gYcZXO5vXO5DwhEmxC9KcATPr8u
-SbmFtRZaqhULWU56CFEG1qFMI2+6qwB8jJ+tWE3U6PeUv6vaufgqVZIoC9KNYzfdsLdYfJYyXv/F
-a7swMAajfa2+2h4UjNrgOVfQi1n8HZIYJe93JHA/X2PVZEKZHfIJZKpklcvh71EAt+2aWWTpMGA8
-7IXoki+IWR9gtufVKhy9Yp8iQXrKRQSL820olxHkOGOxlpbM5kPvE7QE3cc9UFg/eZ5fcVDldiOr
-we3tvVQNrb7gnluDnR9B9AGJwQLIOJ6J47NCqQMxycquHAn/da+q/rteLpfbNtvfNlKrDwdKcdLX
-KM6lq+nVUNI9FqT0bU35Ok1A2WG53+W5Agmt+gzFGqwYV6hz1uI74ITwTQSp3IVhgTSKOoREREDk
-WzV6fUrIYJBMq9JwFViuey+U3XMgsxyRgyIpMGqrG/jXZzFmY3/f/npPd+ykNGKQHO+p6khpxaB/
-Je+0SMf1r3ye9nLa4f/9HyASnPvWDQAA
+H4sIAEa/hVcAA53WNw7ExhUA0F6nUE9YzEuyMCDmnDM7cplzTqf32icQPMUMPqb7eD9sW/2vdUv/
+pH+HQY03ZeHni/D/DTnappnfy9I2iyD2s98frUrppx0w2OtlyLQKwlXwY1k9ysFTDVNSth++SPed
+Ty/abGq7RNsIAwBOkRi0j/dJM8R0tRDe08hyFqLDLChl1TsP+xlW2OSVzPo1i1Q1qWxjPMtu9UD3
+anrPqXjyUa5PEcHxzCvlc7S7maA62Dx64A97RtSjlOscbrQt68/33t118HNvEEzq+8yOTRNEmF9g
+8L4EMtFTA65iJw4mDH44Y5A8dbAudnAEmLuLhnU/coYfOoo7uD8XdVFecnNPgZUF1aNdVn4QDRoC
+yXTaXjGsFRTIQJ7SJAjkr1KEBTTVJ1Lw+HxemvSexE0zRPSchwvm3w9gPvCMy1fkQReQdx7oYH0T
+MdsdnBWhfJG9ZSoatNDXm4/DRzYeUEWQ7ZA7ZSaxNGiDKmBiWTNWFtUS0a2oIZqxtdSW7K+3u4CU
+YuTWwiIYhY8kE7o2NLwv5ED8N5XdQgo/QFVRw9d9cCMi2zuvjKce0jcLHb/c6LgvzJvlx4JKg7js
++Au289bthhN+nyOiC5Ew4Vdt7Jx744vVk7lCggzpIQqm1k9AQ62gJqP3BJLOlkPgD4aDlbDaacKb
+Ijg4WbHoVTeFqmBq4OhAvWJ0eieNkWJi1aFmXdVO0QIEe9YiFNZrdJGPjEVNiVrDNi9Zuh9DaEdD
+m8IeJF3hEZ1yBq9Ghxc8nkVs3D4oFNj//vefeX+ma5Vuf297OpbTmv9V5Mcf2z+izjFfeOW164wI
++WSWwRK4qM1AWzcDAoRx28X3esX0QSsC48NZGSIstqf6VxCDyGSFOYGbq9nSwr3rH2WmqrohpeSB
+2jEGuNvN07UhxsL/HC6TCCieemTRy+Oozxu3ioO9YxKUVMYwvHLDlUSUo3aLnX4VaVQSyp4c9i5U
+hDNXLJtl5iR7gx2OmD6KuF0ouThrwJE/Dymf8S61klEPHcN6A86Aw7WSilMcCD8JMeQWDfUextZ6
+zwGHCdsC0FHFORaTvIErLp8nWSFyj+2u9xPsRdfQFPgrC8hpapDTHMySexs1o9XaeSEF+HwOBf7e
+s6F1PYllqcPleebUumkXYopsBBOwwMhLdN60tF47eP82Kp3rrIejLKVBQVe8NV7dYjHZBXtE9WH1
+4+RC9oIB+GePSae1+BjRcnFa4MLWaYGWHHNE/ZwQFLiPLt/R3ibh+bi0u8x0070GoxvBOA/WYIW4
+EO8ih5KC44tbkThaYBW5oMkIfWfy1PYARmIyi5CM8WyV24KyqjWcRRsAq+WS7peB/QXgim1D+DKW
+q7sIHhZP2mR/mLkgfSgC5Zzl94z1XHXTIpLGW3pp+t4/HF5iTcnOOnJ1nmaEQ0R6G6q7PW3oTTt5
+4hUmn/bdiMBCMaUWHa0hBRE+jqLJ2QqB9BlGmJjnBCAWZGTPWxpJbw9aadwYOsW5/kd9q9f/hzn7
+Zl9sYL+AFhNxuYfxoxasP9XMSHMfBVj6HDhBqme0xnWRXyO6MuQeeysW7OPTQBQ5hHHtUmAYj60v
+xFqwVfAwm6y/fa4tjfC3VVStBb+ntLWqMcvUQbUgFS5N1ecsk+0SmiAQGVSs8FPLFZyoXoDPAbN/
+8SApgAe31ohy9s6gSwnrfUGQE3tfpG+d1ZAxEjjThTi61MsFcMVIPLamAMFAlkKKWAAL0icYMUhW
+9IHpWbIJ+GAmAtcVopizNgqfSUHlUxDQPVh/s4ZspXEJ17WgpTwgKak56oa7dzmsWCq6hjPEdtBo
+aS7s6jA1k77kRQzAUIpJkRAxWUDRlq6+xkqYe7V9um70gpK7LD2IMx+T4Am7z+gyL0mmxjFgNV9o
+6ZOBacJESfTxfs3tKIPBQ/tis+BAzg3p2mfgmhpLQAnWQJcdkRoOxwSddsNvTZX31KMV/oJdtHZZ
+OKhSPwUmyQMMLpCz59GPQN8+3Mdkebs6akI8ZFs+BAH6J+W694oHlmVI5F3V6rMk4jlFNfm5AUjU
+2Ak/cwVRgvPLdEuTikca4tyHcFGIC4Fsrh0QLdLa7vOI809ALTHre/Hnh+w2WPDI4RnOr2p794yI
+ohs1RQoSMxyVxqfypKFE6p0yS9MUrAepe5YoJMfYLUBe0o/WfdH5mIzdp5kGfd4rG+jq7eET2QMH
+jv7LfK+b7O/v9tc/lc7I1++uePoJKynCgwS0iJ+J7bwSeIowge/k2x385bOrh3LMaR0RwEbe3lhU
+DGH5OskgxrdhwGQG3EZFuiGLp/d0aH6794NXo1h+rifbnpaBtsGGjCnoCcns+vBE28Q0f9Ox+lUq
+0FCLjGUX148SpXt0YiPefMfr7GudZ9wJTTtPDVVMHeqqcT2ixzWRaQ8tjSkt+dxG/ngXfeHOVo5K
+J+DLC5vUcJOv3eIbRH0+grRkS9qCkuSiWALeMrm3qU876s6N4b4ASveEnIzWLIS3Dn1nzWxgh27D
+25vcMV/JXsFaTHoJyiSI86ISeJ0oCePIennG9dQbzS/VJaAIo/50E8044rJBmkb2uL+RTRj82gbg
+6THT/ZYQw+8ZAJ6mbLTVtAaQ3S6ZZFFjG1JN6WrW/MVOQzVL2xeLX2JOq1GUNd+BE60qBZaSz+Bx
+nehACGFZZzPZUljKotOOuvbNSHpbDgV2lszhVI4Fqk20jI4zZOio/UC37JPr8eCrG2kdl1iVMCIr
+WlVUj9mXsBmm1HVq+jKsh99ffUzYMihXAtMcdZX8sTNxqq6rclOqT/f7cu6vucO40LJ2l8Bg4Ea0
+8XlmSCfJAWYAZcPm+I7EQYLLfijunrYuZkJn4MgtCBnBDijX7lNnp9E/ny1pzPT+Hkohtp7tcHbk
+iZfs0TjrD80Flt/OcwsPxfQaifdU/ylP+7/nbfvrOw3/CPfmzGo7Tp7XnU7hN1AirjtqJSUwhmke
+KvygceuNpLgQr5pwWCH5EH7GiJRm4qScfrMZY8ld8BPp87SZwyBKcTlyTr5nYbiB9H0f3R0bdoU2
+4O3VK5kE+9EEGYgCuBs1iRL7+Uhd65CJJNuvXeUuHsWkOKQgIWMhhojTnltKNkIV3iNEFInIzMV9
+PrsFEzMYlVzTBfeyHj4cBeUXEKEyRVXt9tBqxzn4Vr4jOFu9TcMtWxHDVrDTg3l1dNPz88RPDN6J
+jDN3yd5EJnLHDv06ZOYJy4Eco2246LAA7/n4zdm7UphbW0UsHTyE5A4nyBeWcYaZmRjgVygANKTD
+YiIC3csg81R6O8YaytqjyG1BFU9j2TFzlz3DJI0oUfZLmL8ibj1vw9pHDNPPb3k6LUOl63nZwtxr
+rE2k533meapOeNkVPV9OR6etwJW8Sph17Q9BeLpIALRwZv1lBRE0xjkqtaMMb+Vm2DixWuX36+YK
+1sU6rk4Ly1RUylh9o9n0K9bzxQ+RXdxaI5Q4rH0G6aWIjReoipJcKsk6uLS6iuuPQ5n9DzzXMgGa
+E0wlii247cX5wWGP5tG+DrX7AV4D6DyK8WQsFlK9Gtr1FWeIcbiFQ3FeenGQ4lQ1UqfPprQiDS6A
+yhDwtf5tDz2g3UwYcDogiN/8M4nA24Q2IYr+FFpupSNXQPzoh/vbp8/wdz99u5/waa3++A8bqQPp
+ew4AAA==
 EOF
 }
 
-function CreateUser {
+function createuser {
 	useradd -r -m ${1}
 
 	mkdir -p /home/${1}/.ssh
-	WriteKeys /home/${1}/.ssh/authorized_keys
+	writekeys /home/${1}/.ssh/authorized_keys
 
 	chown -R ${1}:$(id -g ${1}) /home/${1}/.ssh
 	chmod 700 /home/${1}/.ssh
@@ -67,44 +68,64 @@ function CreateUser {
 }
 
 # Create the two LOCKSS users
-echo " * Creating user lockss..."
-CreateUser 'lockss'
-echo " * Creating user lcap..."
-CreateUser 'lcap'
+createuser 'lockss'
+createuser 'lcap'
 
 # Allow lcap user to sudo
 echo "lcap ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/lcap
 
-# Install the LOCKSS RPM repository
-echo " * Adding LOCKSS RPM repository..." 
+# Write LOCKSS repository information into the Yum repository configuration 
+# directory
 cat > /etc/yum.repos.d/lockss.repo <<'EOF'
 [lockss] 
 name = LOCKSS Daemon Repository 
 baseurl=http://www.lockss.org/repo/ 
 gpgcheck=1
 EOF
-
-# Ensure the correct filesystem permissions on the repository file
 chmod 644 /etc/yum.repos.d/lockss.repo
 
 # Import the LOCKSS GPG key
-echo " * Importing LOCKSS GPG key..."
 rpm --import http://www.lockss.org/LOCKSS-GPG-RPM-KEY
 
-# Install the LOCKSS daemon, OpenJDK and supporting tools
-echo " * Installing software..."
-yum -y -q install lockss-daemon java-1.8.0-openjdk tmux yum-cron 
+# Install the LOCKSS daemon, OpenJDK, and supporting tools
+#   lockss-daemon - LOCKSS software
+#   java-<vers>=openjdk - Java runtime
+#   bind-utils - nslookup, host, etc
+#   dstat - dstat command
+#   git - git command
+#   iotop - iotop command
+#   lshw - lshw command
+#   lsof - lsof command
+#   lynx - text-based browser
+#   mailx - mail command
+#   nmap - network diagnostics
+#   ntp - network time syncronization
+#   pciutils - PCI utilities
+#   rsync - rsync command
+#   smartmontools - smartctl, etc
+#   sysstat - iostat, vmstat, etc
+#   tmux - multi-session tool
+#   wget - wget command
+#   yum-cron - automatic CentOS software updater
+yum -y install lockss-daemon java-1.8.0-openjdk bind-utils dstat git iotop lshw lsof lynx mailx nmap ntp pciutils rsync smartmontools sysstat tmux wget yum-cron
 
-# Enable LOCKSS and other services
-echo " * Enabling lockss service..."
+# Other tools for internal servers
+# yum -y install emacs-nox zip unzip 
+
+# Configure yum-cron for automatic updates
+sed -i 's|^update_messages = no|update_messages = yes|' /etc/yum/yum-cron.conf
+sed -i 's|^download_updates = no|download_updates = yes|' /etc/yum/yum-cron.conf
+sed -i 's|^apply_updates = no|apply_updates = yes|' /etc/yum/yum-cron.conf
+sed -i 's|^emit_via = stdio|emit_via = email|' /etc/yum/yum-cron.conf
+
+# Add LOCKSS to the list of services and enable LOCKSS, and ntpd
+# to start on boot
 systemctl enable lockss
-echo " * Enabling yum-cron service..."
 systemctl enable yum-cron
+systemctl enable ntpd
 
-# Append firewall rules
-echo " * Adjusting firewall..."
+# Inject iptables rules for LOCKSS ports 
 firewall-cmd --zone=public --add-port=8080-8086/tcp --permanent
 firewall-cmd --zone=public --add-port=9729/tcp --permanent
-
-echo "* Done."
+firewall-cmd --reload
 
